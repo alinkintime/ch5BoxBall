6c21ccbb33e840f643ed98df32fbba5576c9402c
Chad Pearson <G00254194@stu.raritanval.edu>
2017-10-23 14:14:49 -0400

    boxBounce method and BoxBall class
    Created the boxbounce method to create a number of balls of random speeds, position, and color to bounce around the box. Also made a boxball class to create a ball with entirely user defined starting atributes.

--- BallDemo.java
+++ BallDemo.java
@@ -1,8 +1,16 @@
 import java.awt.Color;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
+ * 
+ * @author Chad Pearson
+ * @version 2017-October
  *
  * @author Bill Crosbie
  * @version 2015-March-BB
@@ -35,7 +43,7 @@ public class BallDemo
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
+        // create and show the balls
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
         BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
@@ -53,4 +61,49 @@ public class BallDemo
             }
         }
     }
+    
+        /**
+     * Simulates bouncing balls in a box
+     */
+    public void boxBounce(int numberOfBalls)
+    {
+        myCanvas.setVisible(true);
+        // draw the box
+        Rectangle box = new Rectangle(50,50,300,300);
+        myCanvas.draw(box);
+        // crate and show the balls
+        Random random = new Random();
+        HashSet balls = new HashSet();
+        for(int i=0; i<numberOfBalls; i++) {
+            Dimension size = myCanvas.getSize();
+            int x = (int) box.getX() + random.nextInt((int) box.getWidth());            
+            int y = (int) box.getY() + random.nextInt((int) box.getHeight());
+            int xSpeed = random.nextInt(30);
+            int ySpeed = random.nextInt(30);
+            Color color = new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256));
+            BoxBall ball = new BoxBall(x, y, xSpeed, ySpeed, 16, color, box, myCanvas);
+            balls.add(ball);
+            ball.draw();
+        }
+        // make them bounce
+        boolean finished =  false;
+        while(!finished) {
+            myCanvas.wait(50);           // small delay
+            Iterator it = balls.iterator();
+            while(it.hasNext()) {
+                BoxBall ball = (BoxBall) it.next();
+                ball.move();
+                // stop once ball has travelled a certain distance on x axis
+                if(ball.getXPosition() >= 550 + 32*numberOfBalls) {
+                    finished = true;
+                }
+            }
+        }
+        Iterator it = balls.iterator();
+        while(it.hasNext()) {
+            BoxBall ball = (BoxBall) it.next();
+            ball.erase();
+        }
+    } 
+    
 }
